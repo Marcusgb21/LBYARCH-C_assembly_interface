74999a7a9c489e6ca627c86249468d42ee5b5cba
Hosea Marcus Calalang <64012244+Marcusgb21@users.noreply.github.com>
2024-04-01 19:58:55 +0800

    Updated dotProduct.c
    changed arrays to pointers.

--- dotProduct.c
+++ dotProduct.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 
-double dotProduct(double vectorA[], double vectorB[], int size) {
+double dotProduct(double *vectorA, double *vectorB, int size) {
     double result = 0.0;
    
     for (int i = 0; i < size; i++) {
